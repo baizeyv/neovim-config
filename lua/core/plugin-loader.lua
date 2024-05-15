25bcaf64a4ea83aae9a048dcd1afb6fc81eb4daf
baizeyu <baizeyu@doodlemobile.com>
2024-05-15 18:06:19 +0800

    set lazy.nvim

--- lua/core/plugin-loader.lua
+++ lua/core/plugin-loader.lua
@@ -63,7 +63,7 @@ M.lazy_config = {
         -- install missing plugins on startup. This doesn't increase startup time.
         missing = true,
         -- try to load one of these colorschemes when starting an installation during startup
-        colorscheme = { "tokyonight" },
+        colorscheme = { "tokyonight-storm" },
     },
     ui = {
         -- a number <1 is a percentage., >1 is a fixed size
