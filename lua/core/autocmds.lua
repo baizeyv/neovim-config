d8ecea355d3774d72427bfb2ea44b6d136dc5aec
baizeyu <baizeyu@doodlemobile.com>
2024-05-16 17:11:40 +0800

    add some custom autocmds

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -1,7 +1,20 @@
 local M = {}
 
+local function augroup(name)
+    vim.api.nvim_create_augroup("custom_" .. name, { clear = true })
+end
+
 M.setup = function ()
-    
+    -- highlight on yank
+    vim.api.nvim_create_autocmd("TextYankPost", {
+        group = augroup("highlight_yank"),
+        callback = function ()
+            vim.highlight.on_yank({
+                higroup = "IncSearch",
+                timeout = 300
+            })
+        end
+    })
 end
 
 return M
